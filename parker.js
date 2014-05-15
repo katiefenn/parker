4293b2711ca63188785ad3ca9c20a5e7c9197a96
katiefenn <masqurinwizard@googlemail.com>
2014-05-15 22:24:47 +0100

    Overhauled parker.js file.

--- parker.js
+++ parker.js
@@ -9,85 +9,87 @@
  */
 
 var _ = require('underscore'),
-    Parker = require('./lib/Parker.js'),
-    metrics = require('./metrics/All.js'),
-    formatters = require('./lib/formatters'),
-    clc = require('cli-color'),
+    Parker = require('./lib/Parker'),
+    CliController = require('./lib/CliController'),
+    metrics = require('./metrics/All'),
+    formatters = require('./lib/Formatters'),
     argv = require('minimist')(process.argv.slice(2)),
     fs = require('fs'),
     async = require('async'),
     path = require('path'),
-    info = require('./lib/Info.js');
+    info = require('./lib/Info');
 
+var cliController = new CliController(),
+    parker = new Parker(metrics),
+    formatter = formatters['human'];
 
-var format = argv.format || 'human';
-var foutput = formatters[format];
-
-if (!foutput) {
-    console.error('Unknown output format: %s', argv.format);
-    console.error('  available: ' + Object.keys(formatters).join(' '));
-    process.exit(1);
-}
-if (format === 'human') {
-    console.log(clc.red('PA') + clc.yellow('RK') + clc.green('ER') + '-JS');
-}
-
-var parker = new Parker(metrics);
-
-if (argv.h || argv.help) {
-    info.help();
-    process.exit();
-}
-
-if (argv.V || argv.version) {
-    info.version();
-    process.exit();
-}
-
-if (argv._.length > 0) {
+cliController.on('runPaths', function (filePaths) {
     var stylesheets = [];
-
-    async.each(argv._, function (filename, done) {
-        var onComplete = function (err, data) {
+    async.each(filePaths, function (filePath, onAllLoad) {
+        var onFileLoad = function (err, data) {
             stylesheets.push(data);
         };
 
-        if (filename.indexOf('.css') === -1) {
-            fs.readdir(filename, function (err, files) {
-                async.each(files, function (file, fileDone) {
-                    if (file.indexOf('.css') === -1) {
-                        return fileDone();
-                    }
-
-                    fs.readFile(path.join(filename, file), {encoding: 'utf8'}, function (err, fileData) {
-                        onComplete(err, fileData);
-                        fileDone();
-                    });
-                }, done);
-            });
+        if (!fileIsStylesheet(filePath)) {
+            readDirectory(filePath, onFileLoad, onAllLoad);
         }
         else {
-            fs.readFile(filename, {encoding: 'utf8'}, function (err, fileData) {
-                onComplete(err, fileData);
-                done();
-            });
+            readFile(filePath, function (err, data) { onFileLoad(err, data); onAllLoad();});
         }
+
     }, function (err) {
-        var results = parker.run(stylesheets);
-        console.log(foutput(metrics, results));
+        runReport(stylesheets, metrics);
     });
-}
-else {
-    process.stdin.resume();
-    process.stdin.setEncoding('utf8');
-    var stdinData = '';
+});
 
-    process.stdin.on('data', function(chunk) {
-        stdinData += chunk;
-    });
+cliController.on('showVersion', function () {
+    info.version();
+    process.exit();
+});
 
-    process.stdin.on('end', function() {
-        var results = parker.run(stdinData);
-        console.log(foutput(metrics, results));
+cliController.on('showHelp', function () {
+    info.help();
+    process.exit();
+});
+
+cliController.on('setFormat', function (format) {
+    formatter = formatters[format];
+
+    if (!formatter) {
+        console.error('Unknown output format: %s', argv.format);
+        console.error('  available: ' + Object.keys(formatters).join(' '));
+        process.exit(1);
+    }
+});
+
+var readDirectory = function (directoryPath, onFileLoad, onAllLoad) {
+    fs.readdir(directoryPath, function (err, files) {
+        async.each(files, function (file, fileDone) {
+            if (!fileIsStylesheet(file)) {
+                return fileDone();
+            }
+
+            readFile(path.join(directoryPath, file), function(err, fileData) {
+                onFileLoad(err, fileData);
+                fileDone();
+            });
+        }, onAllLoad);
     });
-}
+};
+
+var readFile = function (filePath, onLoad) {
+    fs.readFile(filePath, {encoding: 'utf8'}, function (err, fileData) {
+        onLoad(err, fileData);
+    });
+};
+
+var fileIsStylesheet = function (filePath) {
+    return filePath.indexOf('.css') !== -1;
+};
+
+var runReport = function (stylesheets, metrics) {
+    var results = parker.run(stylesheets);
+    console.log(formatter(metrics, results));
+};
+
+cliController.dispatch(argv);
